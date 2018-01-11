452f939dca44553fb1560962535eb6145eac730a
Spencer Bruce <spencer.bruce@hpe.com>
2018-01-11 17:06:45 +0200

    Fixing tests: Defects Including updating correct query for severity and priority

--- test/integration/defects.js
+++ test/integration/defects.js
@@ -30,6 +30,9 @@ describe('[defects]', function () {
   var client
   var defectID
   var defectName = 'defect test'
+  var workItemRoot
+  var allSeverities
+  var allPhases
 
   before('initialize the Octane client', function (done) {
     var self = this
@@ -43,84 +46,70 @@ describe('[defects]', function () {
         self.skip()
       } else {
         client = aClient
-        done()
+
+        try {
+          createFiveDefects(function () {
+            done()
+          })
+        } catch (error) {
+          msg = err.message
+          console.log('Aborted - %s',
+            typeof msg === 'string' ? msg : JSON.stringify(msg)
+          )
+          self.skip()
+        }
       }
     })
   })
 
-  it('should successfully get all defects list', function (done) {
-    client.defects.getAll({limit: 2}, function (err, defects) {
-      assert.equal(err, null)
-      assert(defects.meta.total_count >= 0)
-      done()
-    })
-  })
-
-  it('should successfully get defects list with limit', function (done) {
-    client.defects.getAll({limit: 10}, function (err, defects) {
-      assert.equal(err, null)
-      if (defects.length === 10) {
-        assert(defects.meta.total_count >= 10)
-      } else {
-        assert(defects.length === defects.meta.total_count)
-      }
-      done()
-    })
-  })
-
-  it('should successfully get defects list with filter', function (done) {
-    var q = Query.field('severity').equal(Query.field('name').equal('severity'))
-    client.defects.getAll({query: q}, function (err, defects) {
-      assert.equal(err, null)
-      defects.forEach(function (defect) {
-        assert.strictEqual(defect.severity.name, 'Low')
-      })
-      done()
-    })
-  })
-
-  it('should successfully get defects list with order', function (done) {
-    client.defects.getAll({order_by: 'id'}, function (err, defects) {
-      assert.equal(err, null)
-      for (var i = 1, l = defects.length; i < l; i++) {
-        assert(defects[i - 1].id < defects[i].id)
-      }
-      done()
-    })
-  })
-
-  it('should successfully create a defect', function (done) {
+  function createFiveDefects(callback) {
     client.workItemRoots.getAll({limit: 1}, function (err, workItemRoots) {
       assert.equal(err, null)
       assert.equal(workItemRoots.length, 1)
-      var workItemRoot = workItemRoots[0]
+      workItemRoot = workItemRoots[0]
 
       client.severities.getAll({}, function (err, severities) {
         assert.equal(err, null)
         assert(severities.length > 0)
-        var severity = severities[0]
+        allSeverities = severities
 
         var q = Query.field('entity').equal('defect')
         client.phases.getAll({query: q}, function (err, phases) {
           assert.equal(err, null)
           assert(phases.length > 0)
-          var phase = phases[0]
+          allPhases = phases
 
-          var defect = {
-            name: defectName,
-            parent: workItemRoot,
-            severity: severity,
-            phase: phase
+          for (var loop = 0; loop < 5; ++loop) {
+            var defect = {
+              name: 'defect' + loop,
+              parent: workItemRoot,
+              severity: allSeverities[loop],
+              phase: allPhases[loop]
+            }
+            client.defects.create(defect, function (err, defect) {
+              assert.equal(err, null)
+              assert(defect.id)
+            })
           }
-          client.defects.create(defect, function (err, defect) {
-            assert.equal(err, null)
-            assert(defect.id)
-
-            defectID = defect.id
-            done()
-          })
+          callback();
         })
       })
+    })
+  }
+
+  it('should successfully create a defect', function (done) {
+    var defect = {
+      name: defectName,
+      parent: workItemRoot,
+      severity: allSeverities[0],
+      phase: allPhases[0]
+    }
+    client.defects.create(defect, function (err, defect) {
+      assert.equal(err, null)
+      assert(defect.id)
+
+      defectID = defect.id
+      done()
     })
   })
 
@@ -140,8 +129,10 @@ describe('[defects]', function () {
       assert.equal(err, null)
       assert(defect)
 
-      assert.strictEqual(defect.name, name)
-      done()
+      client.defects.get({id: defectID, fields: 'name'}, function (err, defect) {
+        assert.strictEqual(defect.name, name)
+        done()
+      })
     })
   })
 
@@ -149,6 +140,61 @@ describe('[defects]', function () {
     client.defects.delete({id: defectID}, function (err, defect) {
       assert.equal(err, null)
       done()
+    })
+  })
+
+  it('should successfully get all defects list', function (done) {
+    client.defects.getAll({}, function (err, defects) {
+      assert.equal(err, null)
+      assert(defects.meta.total_count === 5)
+      done()
+    })
+  })
+
+  it('should successfully get defects list with limit', function (done) {
+    client.defects.getAll({limit: 3}, function (err, defects) {
+      assert.equal(err, null)
+      assert(defects.meta.total_count === 5)
+      assert(defects.length === 3)
+      done()
+    })
+  })
+
+  it('should successfully get defects list with filter', function (done) {
+    var q = Query.field('severity').equal(Query.field('id').equal('severity'))
+    client.defects.getAll({query: q}, function (err, defects) {
+      assert.equal(err, null)
+      defects.forEach(function (defect) {
+        assert.strictEqual(defect.severity.name, 'Low')
+      })
+      done()
+    })
+  })
+
+  it('should successfully get defects list with order', function (done) {
+    client.defects.getAll({order_by: 'id'}, function (err, defects) {
+      assert.equal(err, null)
+      for (var i = 1, l = defects.length; i < l; i++) {
+        assert(defects[i - 1].id < defects[i].id)
+      }
+      done()
+    })
+  })
+
+  after('Delete all defects', function (done) {
+    client.defects.getAll({fields: 'id'}, function (err, defects) {
+      assert.equal(err, null)
+      var promises = []
+      for (var defect of defects) {
+        promises.push(new Promise(function (resolve) {
+          client.defects.delete({id: defect.id}, function () {
+            resolve()
+          })
+        }))
+      }
+      Promise.all(promises).then(function () {
+        done()
+      })
     })
   })
 })
